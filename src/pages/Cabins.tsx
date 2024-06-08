b643e5e76b1068cedeb1659d561811a7b0c0ef3d
elBanna00 <moaazibrahim3@gmail.com>
2024-06-09 02:18:36 +0300

    Feat : add filtering and sorting on cabin table

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -29,17 +29,41 @@
 
 // export default Cabins;
 
+// import CabinTable from "../features/cabins/CabinTable";
+// import Heading from "../ui/Heading";
+// import Row from "../ui/Row";
+// import AddCabin from "../features/cabins/AddCabin";
+
+// function Cabins() {
+//   return (
+//     <>
+//       <Row type="horizontal">
+//         <Heading as="h1">All cabins</Heading>
+//         <p>Filter / Sort</p>
+//       </Row>
+
+//       <Row>
+//         <CabinTable />
+//         <AddCabin />
+//       </Row>
+//     </>
+//   );
+// }
+
+// export default Cabins;
+
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row>
