89dcfef4fe31d2c95f9decd1c6e037e1dc1c8f5e
elBanna00 <moaazibrahim3@gmail.com>
2024-06-10 00:03:20 +0300

    style : adjust ui styled component
    - add flag component
    - migrate buttontext , dataitem , tag to typescript
    - adjust empty and sidebar components

--- src/ui/DataItem.tsx
+++ src/ui/DataItem.tsx
@@ -1,3 +1,4 @@
+import { ReactElement, ReactNode } from "react";
 import styled from "styled-components";
 
 const StyledDataItem = styled.div`
@@ -20,7 +21,15 @@ const Label = styled.span`
   }
 `;
 
-function DataItem({ icon, label, children }) {
+function DataItem({
+  icon,
+  label,
+  children,
+}: {
+  icon: ReactElement;
+  label: string;
+  children: ReactNode;
+}) {
   return (
     <StyledDataItem>
       <Label>
