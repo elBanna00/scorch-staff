fdd1e6c32fde8e183efd75e52d57ccec61bac95e
elBanna00 <moaazibrahim3@gmail.com>
2024-06-10 23:53:36 +0300

    feat:Add new Route for single booking details

--- src/features/bookings/BookingTable.tsx
+++ src/features/bookings/BookingTable.tsx
@@ -1,3 +1,49 @@
+// import BookingRow from "./BookingRow";
+// import Table from "../../ui/Table";
+// import Menus from "../../ui/Menus";
+// import Empty from "../../ui/Empty";
+
+// import { useBookings } from "./useBookings";
+// import Spinner from "../../ui/Spinner";
+// import { BookingWithDetails } from "./BookingDataBox";
+// import Pagination from "../../ui/Pagination";
+
+// function BookingTable() {
+//   const { bookings, isLoading } = useBookings();
+
+//   if (isLoading) return <Spinner />;
+
+//   if (!bookings?.length) return <Empty resourceName="bookings" />;
+
+//   return (
+//     <Menus>
+//       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+//         <Table.Header>
+//           <div>Cabin</div>
+//           <div>Guest</div>
+//           <div>Dates</div>
+//           <div>Status</div>
+//           <div>Amount</div>
+//           <div></div>
+//         </Table.Header>
+
+//         <Table.Body
+//           data={bookings}
+//           render={(booking: any) => (
+//             <BookingRow key={booking.id} booking={booking} />
+//           )}
+//         />
+
+//         <Table.Footer>
+//           <Pagination count={count} />
+//         </Table.Footer>
+//       </Table>
+//     </Menus>
+//   );
+// }
+
+// export default BookingTable;
+
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
@@ -5,11 +51,10 @@ import Empty from "../../ui/Empty";
 
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
-// import { BookingWithDetails } from "./BookingDataBox";
-// import Pagination from "../../ui/Pagination";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
 
@@ -34,7 +79,9 @@ function BookingTable() {
           )}
         />
 
-        <Table.Footer>{/* <Pagination count={count} /> */}</Table.Footer>
+        <Table.Footer>
+          <Pagination count={count!} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
--- src/features/bookings/useBookings.tsx
+++ src/features/bookings/useBookings.tsx
@@ -1,10 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 // import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -24,7 +25,7 @@ export function useBookings() {
   const sortBy = { field, direction };
 
   //   // PAGINATION
-  //   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   // QUERY
   const {
@@ -32,24 +33,24 @@ export function useBookings() {
     data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  //   // PRE-FETCHING
-  //   const pageCount = Math.ceil(count / PAGE_SIZE);
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count! / PAGE_SIZE);
 
-  //   if (page < pageCount)
-  //     queryClient.prefetchQuery({
-  //       queryKey: ["bookings", filter, sortBy, page + 1],
-  //       queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
-  //     });
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
 
-  //   if (page > 1)
-  //     queryClient.prefetchQuery({
-  //       queryKey: ["bookings", filter, sortBy, page - 1],
-  //       queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
-  //     });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, error, bookings, count };
 }
