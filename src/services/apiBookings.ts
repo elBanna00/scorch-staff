74039625a8c248912dacc4958c97cbfdcf880cd4
elBanna00 <moaazibrahim3@gmail.com>
2024-06-10 23:52:54 +0300

    Add BookingToEdit type to the booking obj in update function

--- src/services/apiBookings.ts
+++ src/services/apiBookings.ts
@@ -1,135 +1,5 @@
-// import { getToday } from "../utils/helpers";
-// import supabase from "./supabaseClient";
-// // import { PAGE_SIZE } from "../utils/constants";
-
-// export async function getBookings() {
-//   const query = supabase
-//     .from("bookings")
-//     .select(
-//       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
-//       { count: "exact" }
-//     );
-
-//   // FILTER
-//   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
-
-//   SORT;
-//   if (sortBy)
-//     query = query.order(sortBy.field, {
-//       ascending: sortBy.direction === "asc",
-//     });
-
-//   // if (page) {
-//   //   const from = (page - 1) * PAGE_SIZE;
-//   //   const to = from + PAGE_SIZE - 1;
-//   //   query = query.range(from, to);
-//   // }
-
-//   const { data, error, count } = await query;
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Bookings could not be loaded");
-//   }
-
-//   return { data, count };
-// }
-
-// export async function getBooking(id: number) {
-//   const { data, error } = await supabase
-//     .from("bookings")
-//     .select("*, cabins(*), guests(*)")
-//     .eq("id", id)
-//     .single();
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Booking not found");
-//   }
-
-//   return data;
-// }
-
-// // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
-// // date: ISOString
-// export async function getBookingsAfterDate(date: Date) {
-//   const { data, error } = await supabase
-//     .from("bookings")
-//     .select("created_at, totalPrice, extrasPrice")
-//     .gte("created_at", date)
-//     .lte("created_at", getToday({ end: true }));
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Bookings could not get loaded");
-//   }
-
-//   return data;
-// }
-
-// // Returns all STAYS that are were created after the given date
-// export async function getStaysAfterDate(date: Date) {
-//   const { data, error } = await supabase
-//     .from("bookings")
-//     .select("*, guests(fullName)")
-//     .gte("startDate", date)
-//     .lte("startDate", getToday());
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Bookings could not get loaded");
-//   }
-
-//   return data;
-// }
-
-// // Activity means that there is a check in or a check out today
-// export async function getStaysTodayActivity() {
-//   const { data, error } = await supabase
-//     .from("bookings")
-//     .select("*, guests(fullName, nationality, countryFlag)")
-//     .or(
-//       `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
-//     )
-//     .order("created_at");
-
-//   // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
-//   // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
-//   // (stay.status === 'checked-in' && isToday(new Date(stay.endDate)))
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Bookings could not get loaded");
-//   }
-//   return data;
-// }
-
-// export async function updateBooking(id: number, obj: any) {
-//   const { data, error } = await supabase
-//     .from("bookings")
-//     .update(obj)
-//     .eq("id", id)
-//     .select()
-//     .single();
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Booking could not be updated");
-//   }
-//   return data;
-// }
-
-// export async function deleteBooking(id: number) {
-//   // REMEMBER RLS POLICIES
-//   const { data, error } = await supabase.from("bookings").delete().eq("id", id);
-
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Booking could not be deleted");
-//   }
-//   return data;
-// }
-
+import { Database } from "../../types/supabase";
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabaseClient";
 // import { PAGE_SIZE } from "../utils/constants";
@@ -155,12 +25,9 @@ interface getBookingsProps {
     field: string;
     direction: string;
   };
+  page: number;
 }
-export async function getBookings({
-  filter,
-  sortBy,
-}: //page
-getBookingsProps) {
+export async function getBookings({ filter, sortBy, page }: getBookingsProps) {
   let query = supabase
     .from("bookings")
     .select(
@@ -178,11 +45,11 @@ getBookingsProps) {
       ascending: sortBy.direction === "asc",
     });
 
-  // if (page) {
-  //   const from = (page - 1) * PAGE_SIZE;
-  //   const to = from + PAGE_SIZE - 1;
-  //   query = query.range(from, to);
-  // }
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
 
   const { data, error, count } = await query;
 
@@ -262,8 +129,8 @@ export async function getStaysTodayActivity() {
   }
   return data;
 }
-
-export async function updateBooking(id: number, obj: any) {
+type BookingToEdit = Database["public"]["Tables"]["bookings"]["Update"];
+export async function updateBooking(id: number, obj: BookingToEdit) {
   const { data, error } = await supabase
     .from("bookings")
     .update(obj)
