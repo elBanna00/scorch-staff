a42de2f23886f0804fd8efeba43db71af986c340
elBanna00 <moaazibrahim3@gmail.com>
2024-06-12 05:19:33 +0300

    feat : add todayactivity to dashboard

--- src/features/check-in-out/CheckoutButton.tsx
+++ src/features/check-in-out/CheckoutButton.tsx
@@ -1,8 +1,16 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
-function CheckoutButton() {
+function CheckoutButton({ bookingId }: { bookingId: number }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
---
+++ src/features/check-in-out/TodayItem.tsx
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import Tag from "../../ui/Tag";
+import { Flag } from "../../ui/Flag";
+import Button from "../../ui/Button";
+import CheckoutButton from "./CheckoutButton";
+
+const StyledTodayItem = styled.li`
+  display: grid;
+  grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
+  gap: 1.2rem;
+  align-items: center;
+
+  font-size: 1.4rem;
+  padding: 0.8rem 0;
+  border-bottom: 1px solid var(--color-grey-100);
+
+  &:first-child {
+    border-top: 1px solid var(--color-grey-100);
+  }
+`;
+
+const Guest = styled.div`
+  font-weight: 500;
+`;
+interface ActivityType {
+  cabinId: number | null;
+  cabinPrice: number | null;
+  created_at: string;
+  endDate: string | null;
+  extrasPrice: number | null;
+  guestId: number | null;
+  hasBreakfast: boolean | null;
+  id: number;
+  isPaid: boolean | null;
+  numGuests: number | null;
+  numNights: number | null;
+  observations: string | null;
+  startDate: string | null;
+  status: string | null;
+  totalPrice: number | null;
+  guests: {
+    fullName: string | null;
+    nationality: string | null;
+    countryFlag: string | null;
+  } | null;
+}
+function TodayItem({ activity }: { activity: ActivityType }) {
+  const { id, status, guests, numNights } = activity;
+
+  return (
+    <StyledTodayItem>
+      {status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
+      {status === "checked-in" && <Tag type="blue">Departing</Tag>}
+
+      <Flag
+        src={guests?.countryFlag as string}
+        alt={`Flag of ${guests?.nationality}`}
+      />
+      <Guest>{guests?.fullName}</Guest>
+      <div>{numNights} nights</div>
+
+      {status === "unconfirmed" && (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+      {status === "checked-in" && <CheckoutButton bookingId={id} />}
+    </StyledTodayItem>
+  );
+}
+
+export default TodayItem;
